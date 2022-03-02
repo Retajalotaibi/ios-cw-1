41a784b8496aab142701886b658ae79c73ef27d3
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2022-03-02 19:16:21 +0300

    added my name

---
+++ cw2-day1/cw2-day1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw2-day1
+//
+//  Created by Retaj Al-Otaibi on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello my name is Retaj")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
